0233dbcbbb049359f6ae07582d9307091ce21779
Kaushiki <152272872+kaushikist@users.noreply.github.com>
2026-09-19 20:12:49 +0530

    Create supabase.js

---
+++ src/js/supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://jrxlhqxphtnhtugaetfw.supabase.co";
+const SUPABASE_KEY = "sb_publishable_UWoPMJOgkulQFw_9sOOPNQ_KuMSnOWV";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+
